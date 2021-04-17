46005b1fcd8dd5ea5c53e0bb5946f33d14d377fe
JimmyFV <jfletesv@hotmail.com>
2021-04-17 19:57:36 +0000

    inicio de funcion y componente

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [state, setState] = useState("Hello World!");
+	const myFunction = () => {
+		console.log("This is an example");
+		setState("Good Bye World!");
+	};
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center mt-5" onClick={myFunction}>
+			<h1>{state}</h1>
 		</div>
 	);
 }
