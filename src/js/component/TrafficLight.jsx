33776823ce0980ddd85d37841a8a09618cac5d6b
JimmyFV <jfletesv@hotmail.com>
2021-05-18 22:49:14 +0000

    fin traffic light

---
+++ src/js/component/TrafficLight.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+//include images into your bundle
+
+//create your first component
+export default class TrafficLight extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			clickLight: null
+		};
+	}
+
+	render() {
+		let redExtraClass = "";
+		if (this.state.clickLight == "red") redExtraClass = "selected";
+		let yellowExtraClass = "";
+		if (this.state.clickLight == "yellow") yellowExtraClass = "selected";
+		let greenExtraClass = "";
+		if (this.state.clickLight == "green") greenExtraClass = "selected";
+		return (
+			<div>
+				<div id="traffictop"></div>
+				<div id="container">
+					<div
+						className={"red light " + redExtraClass}
+						onClick={() =>
+							this.setState({ clickLight: "red" })
+						}></div>
+					<div
+						className={"yellow light " + yellowExtraClass}
+						onClick={() =>
+							this.setState({ clickLight: "yellow" })
+						}></div>
+					<div
+						className={"green light " + greenExtraClass}
+						onClick={() =>
+							this.setState({ clickLight: "green" })
+						}></div>
+				</div>
+			</div>
+		);
+	}
+}
